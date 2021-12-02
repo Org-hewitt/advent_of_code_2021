dd1f0107ba830043e7704445a10216cdb5ee27e5
Robert Hewitt <robert.hewitt@fanduel.com>
2021-12-02 08:37:16 +0000

    refactor of day 2
    reuse the same function for part1&2

--- day2/index.js
+++ day2/index.js
@@ -1,69 +1,37 @@
 #!/usr/bin/env node
 const { readFile } = require('../util/helpers');
 
-const println = (x) => { console.log(x); return x }
-const testFile = "./day2/data_test.txt"
-const realFile = "./day2/data_real.txt"
+const test = "./day2/data_test.txt"
+const real = "./day2/data_real.txt"
 
-const add = (v1, v2) => {
-    const result = []
-    for (i = 0; i < v1.length; i++) result[i] = v1[i] + v2[i]
-    return result
+const vectorReducer = (vectorFactory) => (vOld, sOrder) => {
+    const [move, sUnit] = sOrder.split(" ")
+    const vMove = vectorFactory[move](vOld, parseInt(sUnit))
+    const vResult = vOld.map((v, i) => v + vMove[i])
+    // console.log(vOld, "+", vMove, "=", vResult)
+    return vResult
 }
+
+const run = ({ file, origin, r }) => readFile(file).split("\n").reduce(r, origin).slice(0, 2).reduce((a, b) => a * b)
 
 console.log(`day 2 - START`)
 
-/**
-    forward X increases the horizontal position by X units.
-    down X increases the depth by X units.
-    up X decreases the depth by X units.
- */
-// [horizontal, vertical] == [x,y]
-
-const convertToVector = (value) => {
-    const [word, unitStr] = value.split(" ")
-    const units = parseInt(unitStr)
-    switch (word) {
-        case "forward": return [units, 0]
-        case "down": return [0, units]
-        case "up": return [0, -units]
-    }
+const vectorFactoryXY = {
+    up: (_, unit) => [0, -unit],
+    down: (_, unit) => [0, unit],
+    forward: (_, unit) => [unit, 0]
 }
 
+console.log(`part 1 - T = ${run({ file: test, origin: [0, 0], r: vectorReducer(vectorFactoryXY) })}`)
+console.log(`part 1 - R = ${run({ file: real, origin: [0, 0], r: vectorReducer(vectorFactoryXY) })}`)
 
-const run = ({ file, startAt }) => readFile(file).split("\n")
-    .map(convertToVector)
-    .reduce(add, startAt)
-    .reduce((a, b) => a * b)
-
-console.log(`part 1 - test input result = ${run({ file: testFile, startAt: [0, 0] })}`)
-console.log(`part 1 - real input result = ${run({ file: testFile, startAt: [0, 0] })}`)
-
-/**
-    down X increases your aim by X units.
-    up X decreases your aim by X units.
-    forward X does two things:
-    It increases your horizontal position by X units.
-    It increases your depth by your aim multiplied by X.
- */
-// [horizontal, vertical, aim] == [x,y,z]
-const convertToVector2 = (currentVector, value) => {
-    const [word, unitStr] = value.split(" ")
-    const units = parseInt(unitStr)
-    const [_h, _v, aim] = currentVector
-    switch (word) {
-        case "down": return add(currentVector, [0, 0, units])
-        case "up": return add(currentVector, [0, 0, -units])
-        case "forward": return add(currentVector, [units, aim * units, 0])
-    }
+const vectorFactoryXYZ = {
+    up: (_, unit) => [0, 0, -unit],
+    down: (_, unit) => [0, 0, unit],
+    forward: ([_x, _y, aim], unit) => [unit, aim * unit, 0]
 }
 
-const run2 = ({ file, startAt }) => readFile(file).split("\n")
-    .reduce(convertToVector2, startAt)
-    .reduce((a, v) => { if (a.length < 2) a.push(v); return a }, [])
-    .reduce((a, b) => a * b)
-
-console.log(`part 2 - test input result = ${run2({ file: realFile, startAt: [0, 0, 0] })}`)
-console.log(`part 2 - real input result = ${run2({ file: realFile, startAt: [0, 0, 0] })}`)
+console.log(`part 2 - T = ${run({ file: test, origin: [0, 0, 0], r: vectorReducer(vectorFactoryXYZ) })}`)
+console.log(`part 2 - R = ${run({ file: real, origin: [0, 0, 0], r: vectorReducer(vectorFactoryXYZ) })}`)
 
 console.log(`day 2 - END`)
