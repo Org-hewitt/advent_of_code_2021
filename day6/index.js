48010cbe9fb0a1af8facfb05da12ba88c05c002f
Robert Hewitt <fuzzybear@gmail.com>
2021-12-06 09:58:40 +0000

    day6 (#2)

---
+++ day6/index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+const { readFile } = require('../util/helpers');
+
+const day = 6;
+const test = `./day${day}/data_test.txt`
+const real = `./day${day}/data_real.txt`
+
+const toInt = x => parseInt(x)
+
+const empty = () => [0, 0, 0, 0, 0, 0, 0, 0, 0]
+const run = ({ file, days }) => {
+    let fishies = readFile(file).split(",").map(toInt).reduce((acc, c) => {
+        acc[c] = acc[c] + 1; return acc
+    }, empty())
+    for (let i = 0; i < days; i++) {
+        const spawners = fishies.shift()
+        fishies[6] = fishies[6] + spawners
+        fishies.push(spawners)
+    }
+    return Object.values(fishies).reduce((a, b) => a + b)
+}
+
+console.log(`START - day ${day}\n`)
+console.log(`Pt1 : expect 26 from 18 days : actual = ${run({ file: test, days: 18 })}`)
+console.log(`Pt1 : expect 5934 from 80 days : actual = ${run({ file: test, days: 80 })}`)
+console.log(`Pt1 confirmed: 385391 : result = ${run({ file: real, days: 80 })}\n`)
+
+console.log(`Pt2 : expect 26984457539 : actual = ${run({ file: test, days: 256 })}`)
+console.log(`Pt2 confirmed: 1728611055389 : result = ${run({ file: real, days: 256 })}\n`)
+
+console.log(`END - day ${day}\n`)
