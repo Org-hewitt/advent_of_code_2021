b739a55548cf33e11e4eacb5c8189d0c6362ca96
Robert Hewitt <robert.hewitt@fanduel.com>
2021-12-06 09:58:03 +0000

    better

--- day6/index.js
+++ day6/index.js
@@ -7,39 +7,15 @@ const real = `./day${day}/data_real.txt`
 
 const toInt = x => parseInt(x)
 
-const tick = (c, r) => {
-    if (c === 0) return { daysLeft: r, newFish: true }
-    return { daysLeft: --c }
-}
-
-const breed = ({ input, rNewFish, rOldFish }) => {
-    const { n, f } = input.reduce(({ n, f }, currentFish) => {
-        const { daysLeft, newFish } = tick(currentFish, rOldFish)
-        f.push(daysLeft)
-        return { n: newFish ? n + 1 : n, f }
-    }, { n: 0, f: [] })
-
-    for (let i = 0; i < n; i++) f.push(rNewFish)
-    return f
-}
-
 const empty = () => [0, 0, 0, 0, 0, 0, 0, 0, 0]
 const run = ({ file, days }) => {
     let fishies = readFile(file).split(",").map(toInt).reduce((acc, c) => {
         acc[c] = acc[c] + 1; return acc
     }, empty())
     for (let i = 0; i < days; i++) {
-        fishies = fishies.reduce((acc, val, index, arr) => {
-            if (index === 0) {
-                acc[6] = val + (arr[7] || 0)
-                acc[8] = val
-            } else if (index === 7) { }
-            else {
-                acc[index - 1] = val
-            }
-            return acc
-        }, empty())
-        // console.log(`day ${i + 1} = ${JSON.stringify(fishies)}`)
+        const spawners = fishies.shift()
+        fishies[6] = fishies[6] + spawners
+        fishies.push(spawners)
     }
     return Object.values(fishies).reduce((a, b) => a + b)
 }
