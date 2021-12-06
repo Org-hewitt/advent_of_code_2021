c3932ea2048db3dd6755255d87061e6ce6f7424b
Robert Hewitt <robert.hewitt@fanduel.com>
2021-12-06 09:52:11 +0000

    day6 pt2

--- day6/index.js
+++ day6/index.js
@@ -23,18 +23,33 @@ const breed = ({ input, rNewFish, rOldFish }) => {
     return f
 }
 
+const empty = () => [0, 0, 0, 0, 0, 0, 0, 0, 0]
 const run = ({ file, days }) => {
-    let fishies = readFile(file).split(",").map(toInt)
-    for (let i = 0; i < days; i++) fishies = breed({ input: fishies, rNewFish: 8, rOldFish: 6 })
-    return fishies.length
+    let fishies = readFile(file).split(",").map(toInt).reduce((acc, c) => {
+        acc[c] = acc[c] + 1; return acc
+    }, empty())
+    for (let i = 0; i < days; i++) {
+        fishies = fishies.reduce((acc, val, index, arr) => {
+            if (index === 0) {
+                acc[6] = val + (arr[7] || 0)
+                acc[8] = val
+            } else if (index === 7) { }
+            else {
+                acc[index - 1] = val
+            }
+            return acc
+        }, empty())
+        // console.log(`day ${i + 1} = ${JSON.stringify(fishies)}`)
+    }
+    return Object.values(fishies).reduce((a, b) => a + b)
 }
 
 console.log(`START - day ${day}\n`)
 console.log(`Pt1 : expect 26 from 18 days : actual = ${run({ file: test, days: 18 })}`)
-console.log(`Pt1 : expect 5934 from 80 days : actual = ${run({ file: test, days: 80 })}\n`)
+console.log(`Pt1 : expect 5934 from 80 days : actual = ${run({ file: test, days: 80 })}`)
 console.log(`Pt1 confirmed: 385391 : result = ${run({ file: real, days: 80 })}\n`)
 
-// console.log(`Pt2 : expect xxx : actual = ${run({ file: test })}\n`)
-// console.log(`Pt2 : actual = ${run({ file: real })}\n`)
+console.log(`Pt2 : expect 26984457539 : actual = ${run({ file: test, days: 256 })}`)
+console.log(`Pt2 confirmed: 1728611055389 : result = ${run({ file: real, days: 256 })}\n`)
 
-console.log(`END   - day ${day}\n`)
+console.log(`END - day ${day}\n`)
