5ca227bea193b8dbc742d0ee2055a6e191d5dbcf
Robert Hewitt <robert.hewitt@fanduel.com>
2021-12-06 08:03:17 +0000

    day6 start

---
+++ day6/index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+const { readFile } = require('../util/helpers');
+
+const day = 6;
+const test = `./day${day}/data_test.txt`
+const real = `./day${day}/data_real.txt`
+
+
+const run = ({ file }) => {
+    return ""
+}
+
+console.log(`START - day ${day}\n`)
+console.log(`Pt1 : expect xxx : actual = ${run({ file: test })}\n`)
+console.log(`Pt2 : expect xxx : actual = ${run({ file: test })}\n`)
+
+// console.log(`Pt1 : answer = ${run({ file: real })}\n`)
+// console.log(`Pt2 : actual = ${run({ file: real })}\n`)
+
+console.log(`END   - day ${day}\n`)
