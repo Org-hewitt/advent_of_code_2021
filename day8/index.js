b2b71b803d1686535bed3a1ab9bb1732a3f08b79
Robert Hewitt <robert.hewitt@fanduel.com>
2021-12-08 09:32:22 +0000

    day 8
    part 1 complete
    part 2 - partial / given up

--- day8/index.js
+++ day8/index.js
@@ -7,17 +7,36 @@ const real = `./day${day}/data_real.txt`
 
 const toInt = x => parseInt(x)
 
-const run = ({ file }) => {
-    const s = readFile(file)
+const run1 = ({ file, lookFor }) => readFile(file).split("\n")
+    .map(line => line.split(" | ")[1].split(" "))
+    .reduce((acc, val) => acc.concat(val), [])
+    .filter(s => lookFor.includes(s.length))
+    .length
 
-    return s
+const uniqueLenghts = [2, 3, 4, 7] // 1, 7, 4, 8
+
+const decode = (mapping, aCodes) => {
+    console.log(`here`, mapping, aCodes)
+    return aCodes.map(code => {
+        console.log(`searching for ${code} -> ${mapping[code]}`)
+        return mapping[code]
+    }).reduce((acc, v) => acc + v)
 }
+
+const run2 = ({ file, getMapping }) => readFile(file).split("\n")
+    .map(line => {
+        const [codes, values] = line.split(" | ")
+        const mapping = getMapping(codes)
+        return toInt(decode(mapping, values.split(" ")))
+    })
+
+const mappingLine1TestData = { "acedgfb": "8", "cdfbe": "5", "gcdfa": "2", "fbcad": "3", "dab": "7", "cefabd": "9", "cdfgeb": "6", "eafb": "4", "cagedb": "0", "ab": "1" }
 
 console.log(`START - day ${day}\n`)
 console.log(`PART - 1 : \n`)
-console.log(`test data : expect     xxx : actual = ${run({ file: test })}`)
-console.log(`real data : confirmed: xxx : result = ${run({ file: real })}\n`)
+// console.log(`test data : expect     2  : actual = ${run1({ file: test, lookFor: uniqueLenghts })}`)
+console.log(`real data : confirmed: 303 : result = ${run1({ file: real, lookFor: uniqueLenghts })}\n`)
 console.log(`PART - 2: \n`)
-console.log(`test data : expect     xxx : actual = ${run({ file: test })}`)
-console.log(`real data : confirmed: xxx : result = ${run({ file: real })}\n`)
+console.log(`test data : expect    5353 : actual = ${run2({ file: test, getMapping: () => mappingLine1TestData })}`)
+// console.log(`real data : confirmed: xxx : result = ${run({ file: real })}\n`)
 console.log(`END - day ${day}\n`)
