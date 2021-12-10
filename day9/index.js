1974e6a58ccdb6630bd4d8e71ebf33a417a6ac25
Robert Hewitt <robert.hewitt@fanduel.com>
2021-12-10 20:37:18 +0000

    day 9 re-types

--- day9/index.js
+++ day9/index.js
@@ -6,7 +6,6 @@ const test = `./day${day}/data_test.txt`
 const real = `./day${day}/data_real.txt`
 
 const toInt = x => parseInt(x)
-const toConsole = (x) => { console.log(x); return x }
 
 const getAdjacents = (x, y, rows) =>
     [[x + 1, y], [x - 1, y], [x, y + 1], [x, y - 1]]
@@ -27,24 +26,22 @@ const getBasinFor = ([x, y], rows, inBasin) =>
 
 const calcScorePt1 = (mins, rows) => mins.map(([x, y]) => rows[y][x]).reduce((a, b) => a + b + 1, 0)
 const calcScorePt2 = (mins, rows) => {
-    console.log(`ok, we have ${mins.length} local minimums`)
     return mins.map(min => {
         const basin = [min]
         getBasinFor(min, rows, basin)
         return { point: min, basin }
     })
-        .sort((a, b) => a.basin.length - b.basin.length)
-        .reduce((acc, { basin, point }) => {
-            // console.log(`point: (${point[0]},${point[1]}) -> ${basin.length}`)
-            const v = basin.length
-            if (acc.length == 3) {
-                acc = acc.filter(max => max >= v)
-                if (acc.length !== 3) acc.push(v)
+        .reduce((acc, { basin }) => {
+            if (acc.length === 3) {
+                acc.sort((a, b) => a - b)
+                const lowest = acc.shift()
+                if (basin.length > lowest) acc.push(basin.length)
+                else acc.push(lowest)
             } else {
-                acc.push(v)
+                acc.push(basin.length)
             }
             return acc
-        }, []).map(toConsole).reduce((a, b) => a * b, 1)
+        }, []).reduce((a, b) => a * b, 1)
 }
 
 const run = ({ file, calcScore }) => {
@@ -64,6 +61,6 @@ console.log(`PART - 1 : \n`)
 console.log(`test data : expect      15 : actual = ${run({ file: test, calcScore: calcScorePt1 })}`)
 console.log(`real data : confirmed: 570 : result = ${run({ file: real, calcScore: calcScorePt1 })} \n`)
 console.log(`PART - 2: \n`)
+console.log(`test data : expect       1134 : actual = ${run({ file: test, calcScore: calcScorePt2 })}`)
 console.log(`real data : confirmed: 899392 : result = ${run({ file: real, calcScore: calcScorePt2 })}\n`)
-console.log(`test data : expect    1134 : actual = ${run({ file: test, calcScore: calcScorePt2 })}`)
 console.log(`END - day ${day}\n`)
