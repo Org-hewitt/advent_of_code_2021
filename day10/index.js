8c4294ba2f2ed1fc433d2a20b9debb9d7e9e82a2
Robert Hewitt <robert.hewitt@fanduel.com>
2021-12-10 09:08:55 +0000

    day 10 complete and 11 template

--- day10/index.js
+++ day10/index.js
@@ -11,7 +11,7 @@ const isClosingBracket = ["]", "}", ")", ">"]
 
 const convertToValue = (bracket) => values[bracket]
 
-const convert = (line) => {
+const convert = (line, includeIncomplete) => {
     const input = line.split("")
     const expectedCloses = []
 
@@ -23,6 +23,7 @@ const convert = (line) => {
             expectedCloses.push(closingBrackets[x])
         }
     }
+    if (includeIncomplete && expectedCloses.length > 0) return { incomplete: true, expectedCloses }
 
     return { corrupt: false }
 }
@@ -31,11 +32,26 @@ const run = ({ file }) => {
     const lines = readFile(file).split("\n")
 
     return lines
-        .map(convert)
+        .map(x => convert(x))
         .filter(({ corrupt }) => corrupt)
         .map(({ brokenBracket }) => brokenBracket)
         .map(convertToValue)
         .reduce((a, b) => a + b, 0)
+}
+
+const values2 = { "]": 2, ")": 1, "}": 3, ">": 4, }
+const convertToValue2 = (arr) => arr.reverse().reduce((acc, v) => acc * 5 + (values2[v]), 0)
+
+const run2 = ({ file }) => {
+    const lines = readFile(file).split("\n")
+    const x = lines
+        .map(x => convert(x, true))
+        .filter(({ incomplete }) => incomplete)
+        .map(({ expectedCloses }) => expectedCloses)
+        .map(convertToValue2)
+        .sort((a, b) => a - b)
+
+    return x[(x.length - 1) / 2]
 }
 
 console.log(`START - day ${day}\n`)
@@ -43,6 +59,6 @@ console.log(`PART - 1 : \n`)
 console.log(`test data : expect      26397 : actual = ${run({ file: test })}`)
 console.log(`real data : confirmed: 367059 : result = ${run({ file: real })}\n`)
 console.log(`PART - 2: \n`)
-// console.log(`test data : expect     xxx : actual = ${run({ file: test })}`)
-// console.log(`real data : confirmed: xxx : result = ${run({ file: real })}\n`)
+console.log(`test data : expect     288957 : actual = ${run2({ file: test })}`)
+console.log(`real data : confirmed: xxx : result = ${run2({ file: real })}\n`)
 console.log(`END - day ${day}\n`)
