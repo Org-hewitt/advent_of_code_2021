93d1b527223ced706e3aff6b1b77f60ceb2232a8
Robert Hewitt <robert.hewitt@fanduel.com>
2021-12-11 08:48:11 +0000

    day 11 start

--- day11/index.js
+++ day11/index.js
@@ -5,17 +5,20 @@ const day = 11;
 const test = `./day${day}/data_test.txt`
 const real = `./day${day}/data_real.txt`
 
+const toInt = x => parseInt(x)
+
 const run = ({ file }) => {
-    readFile(file)
+    const input = readFile(file).split('\n').map(s => s.split("").map(toInt))
+    console.log(input)
 
     return ""
 }
 
 console.log(`START - day ${day}\n`)
 console.log(`PART - 1 : \n`)
-console.log(`test data : expect     xxx : actual = ${run({ file: test })}`)
+console.log(`test data : expect    1656 : actual = ${run({ file: test })}`)
 // console.log(`real data : confirmed: xxx : result = ${run({ file: real })}\n`)
-console.log(`PART - 2: \n`)
+// console.log(`PART - 2: \n`)
 // console.log(`test data : expect     xxx : actual = ${run({ file: test })}`)
 // console.log(`real data : confirmed: xxx : result = ${run({ file: real })}\n`)
 console.log(`END - day ${day}\n`)
