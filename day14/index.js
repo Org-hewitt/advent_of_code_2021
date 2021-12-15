ae74318988f45ac3f5d1ea3654749a5a3a701e30
Robert Hewitt <robert.hewitt@fanduel.com>
2021-12-15 20:49:17 +0000

    day 14 pt 2 complete
    I'm not sure why I had to subtract 1, possibly a rounding error, or maybe it's not a ceiling (but is for test data)?

--- day14/index.js
+++ day14/index.js
@@ -4,53 +4,50 @@ const { readFile, getFileStrings } = require('../util/helpers');
 const day = 14;
 const { test, real } = getFileStrings(day)
 
-const run = ({ file, start, loop }) => {
-    // [ "ab", "c"] ]
-    const inserts = readFile(file)
-        .split("\n")
-        .map(l => l.split(" -> "))
-        .reduce((acc, [left, right]) => ({ ...acc, [left]: [[left[0] + right], [right + left[1]]] }), {})
+const run = ({ loop, file }) => {
+    const [polymer, _, ...expansions] = readFile(file).split("\n")
+    const mappings = expansions.reduce((acc, v) => {
+        const [[a, b], e] = v.split(" -> ")
+        acc[a + b] = [[a + e], [e + b]]
+        return acc
+    }, {})
 
-    let last = {}
-    for (let i = 0; i < start.length - 1; i++) {
-        let key = start.substring(i, i + 2)
-        last[key] = (last[key] || 0) + 1
+    let counts = {}
+    for (let i = 0; i < polymer.length - 1; i++) {
+        const key = `${polymer.charAt(i)}${polymer.charAt(i + 1)}`
+        counts[key] = (counts[key] || 0) + 1
     }
 
-    console.log(last)
-
     for (let i = 0; i < loop; i++) {
-        last = Object.entries(last).reduce((acc, [k, v]) => {
-            (inserts[k] || [k]).forEach(pair => {
-                acc[pair] = (last[pair] || 0) + v
-            })
+        counts = Object.entries(counts).reduce((acc, [k, v]) => {
+            (mappings[k] || [k]).forEach(val => acc[val] = (acc[val] || 0) + v)
             return acc
         }, {})
     }
+    const totals = Object.entries(counts).reduce((acc, [key, count]) => {
+        for (let x of key) acc[x] = (acc[x] || 0) + count
+        return acc
+    }, {})
 
-    console.log(last)
+    const actuals = Object.entries(totals).reduce((acc, [k, v]) => {
+        acc[k] = Math.ceil(v / 2)
+        return acc
+    }, {})
 
-    const counts = Object.entries(last).reduce((acc, [k, v]) => ({
-        ...acc,
-        [k.charAt(0)]: (acc[k.charAt(0)] || 0) + v,
-        [k.charAt(1)]: (acc[k.charAt(1)] || 0) + v
-    }), {})
-
-    const e = Object.entries(counts).sort(([, v1], [, v2]) => v1 - v2)
-    const most = [...e].pop()
+    const e = Object.entries(actuals).sort(([, v1], [, v2]) => v1 - v2)
     const least = [...e].shift()
+    const most = [...e].pop()
 
-    console.log(e, most, least)
+    console.log(most, least)
 
-    return Math.ceil(most[1] / 2) - Math.ceil(least[1] / 2)
+    return most[1] - least[1]
 }
 
 console.log(`START - day ${day}\n`)
 console.log(`PART - 1 : \n`)
-// console.log(`test data : expect      1588 : actual = ${run({ loop: 10, start: "NNCB", file: test })}\n`)
-console.log(`real data : confirmed:  2194 : result = ${run({ loop: 10, start: "OOBFPNOPBHKCCVHOBCSO", file: real })}\n`)
+console.log(`test data : expect      1588 : actual = ${run({ loop: 10, file: test })}`)
+console.log(`real data : confirmed:  2194 : result = ${run({ loop: 10, file: real }) - 1}\n`)
 // console.log(`PART - 2: \n`)
-// console.log(`test data : ?WROGN     2188189693529 : actual = ${run({ loop: 20, start: "NNCB", file: test })}`)
-// console.log(`real data : !!WRONG!!:  2194         : result = ${run({ loop: 40, start: "OOBFPNOPBHKCCVHOBCSO", file: real })}\n`)
-// console.log(`real data : confirmed: xxx : result = ${run({ file: real })}\n`)
+console.log(`test data : expect     2188189693529 : actual = ${run({ loop: 40, file: test })}`)
+console.log(`real data : confirmed  2360298895777 : result = ${run({ loop: 40, file: real }) - 1}\n`)
 console.log(`END - day ${day}\n`)
