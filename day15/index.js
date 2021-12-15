cb5bd72ae743bdbdd9f5304f6fd9e180228f0d63
Robert Hewitt <robert.hewitt@fanduel.com>
2021-12-15 19:39:19 +0000

    day 15 pt 1 & 2

--- day15/index.js
+++ day15/index.js
@@ -1,40 +1,12 @@
 #!/usr/bin/env node
 const { readFile, getFileStrings, getAdjacentPoints, toInt } = require('../util/helpers');
+var { find_path } = require('dijkstrajs');
 
 const day = 15;
 const { test, real } = getFileStrings(day)
 
-const a = [1, 2, 3]
-console.log(a, a.shift())
-
-const getShortest = (origin, target, allPoints, path, currentShortest) => {
-    const { edges } = allPoints[origin]
-
-    return edges
-        .filter(e => !path.includes(e))
-        .filter(e => {
-            const risk = path.concat(e).map(p => allPoints[p].weight).reduce((a, b) => a + b)
-            const isSmaller = risk < currentShortest[0]
-            // if (!isSmaller) console.log(`pruning path with length ${path.length} and risk: ${risk}`)
-            return isSmaller
-        })
-        .map(e => {
-            if (e === target) {
-                const fullPath = path.concat(target)
-                const [_origin, ...riskPath] = fullPath // remove origin
-                const risk = riskPath.map(p => allPoints[p].weight).reduce((a, b) => a + b)
-                console.log(`found a route of risk: ${risk} with riskPath of: ${riskPath}`)
-                currentShortest.pop()
-                currentShortest.push(risk)
-                return { path: fullPath, risk }
-            }
-            return getShortest(e, target, allPoints, path.concat(e), currentShortest)
-        }).reduce((a, v) => v.risk < a.risk ? v : a, { risk: 999999999 })
-}
-
-
-const run = ({ file }) => {
-    const lines = readFile(file).split("\n")
+const run = ({ file, convertArray }) => {
+    const lines = convertArray(readFile(file).split("\n"))
     const points = {}
     const maxY = lines.length - 1
     const maxX = lines[0].length - 1
@@ -42,25 +14,68 @@ const run = ({ file }) => {
         const line = lines[y]
         for (let x = 0; x <= maxX; x++) {
             const key = `(${x},${y})`
-            const adjs = getAdjacentPoints({ point: [x, y], maxX, maxY }).map(([x, y]) => `(${x},${y})`)
-            points[key] = { key, weight: toInt(line[x]), edges: adjs }
+            const edges = getAdjacentPoints({ point: [x, y], maxX, maxY })
+                .filter(([_, y1]) => x === maxX ? y1 >= y : true)
+                .filter(([x1, _]) => y === maxY ? x1 >= x : true)
+                .map(([x, y]) => `(${x},${y})`)
+            points[key] = { weight: toInt(line[x]), edges }
         }
     }
 
+    // console.log(points)
+
     const origin = "(0,0)"
     const target = `(${maxX},${maxY})`
-    const answer = getShortest(origin, target, points, [origin], [2000])
 
-    console.log(answer)
+    const graph = {}
 
-    return answer.risk
+    Object.entries(points).forEach(([key, value]) => {
+        const nodesFromKey = {}
+        for (let edge of value.edges) {
+            nodesFromKey[edge] = points[edge].weight
+        }
+        graph[key] = nodesFromKey
+    })
+
+    const r = find_path(graph, origin, target)
+    r.shift() // get rid of origin
+    return r.map(p => points[p].weight).reduce((a, b) => a + b)
+}
+
+const pt1 = (lines) => lines
+const pt2 = (lines) => {
+    const orig = lines.map(line => {
+        let last = line.split("").map(toInt)
+        const output = [...last]
+        for (let i = 0; i < 4; i++) {
+            last = last.map(n => n === 9 ? 1 : ++n)
+            output.push(...last)
+        }
+        return output.reduce((a, v) => `${a}${v}`, "")
+    })
+
+    const output = [...orig]
+    let last = orig
+    for (let i = 0; i < 4; i++) {
+        last = last.map(line => line.split("")
+            .map(toInt)
+            .map(n => n === 9 ? 1 : ++n)
+            .reduce((a, v) => `${a}${v}`, "")
+        )
+        output.push(...last)
+    }
+
+    return output.reduce((a, v) => {
+        a.push(v)
+        return a
+    }, [])
 }
 
 console.log(`START - day ${day}\n`)
 console.log(`PART - 1 : \n`)
-console.log(`test data : expect     40 : actual = ${run({ file: test })}`)
-console.log(`real data : confirmed: xxx : result = ${run({ file: real })}\n`)
-// console.log(`PART - 2: \n`)
-// console.log(`test data : expect     xxx : actual = ${run({ file: test })}`)
-// console.log(`real data : confirmed: xxx : result = ${run({ file: real })}\n`)
+console.log(`test data : expect     40 : actual = ${run({ file: test, convertArray: pt1 })}`)
+console.log(`real data : correct : 748 : result = ${run({ file: real, convertArray: pt1 })}\n`)
+console.log(`PART - 2: \n`)
+console.log(`test data : expect    315 : actual = ${run({ file: test, convertArray: pt2 })}`)
+console.log(`real data : correct: xxx : result = ${run({ file: real, convertArray: pt2 })}`)
 console.log(`END - day ${day}\n`)
